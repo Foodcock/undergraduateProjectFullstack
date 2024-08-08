5bd349c5aa576e72254b97f444e1047791a47ed0
dragon0309 <102803204+dragon0309@users.noreply.github.com>
2024-08-08 17:05:21 +0800

    homePage做到一半

--- front-end/src/views/CartPage.vue
+++ front-end/src/views/CartPage.vue
@@ -1,10 +1,94 @@
 <template>
-  <h1>This is the Cart Page</h1>
+  <div class="body">
+    <div id="grocery-list">
+      <div
+        v-for="item in groceries"
+        :key="item.groceryName"
+        class="grocery-item"
+      >
+        <h2>商品名稱: {{ item.groceryName }}</h2>
+        <img :src="getImageUrl(item.groceryName)" alt="商品圖片" />
+        <p>店名: {{ item.storeName }}</p>
+        <p>折扣價: {{ item.discountedPrice }} 元</p>
+        <p>有效期至: {{ item.expirationDate }}</p>
+        <input type="checkbox" v-model="item.checked" />
+        <button @click="removeItem(index)">刪除</button>
+      </div>
+    </div>
+    <div id="totalPrice">
+      <h2>總價: {{ totalPrice }}</h2>
+    </div>
+    <button @click="toHomePage">繼續選購</button>
+    <button @click="toPayPage">結帳</button>
+  </div>
 </template>
 
 <script>
 export default {
   name: "CartPage",
+  data() {
+    let groceries = [
+      {
+        discount: true,
+        discountedPrice: "87.2",
+        expirationDate: "2025-07-21",
+        groceryName: "飯糰",
+        price: "109",
+        storeName: "7-11長興",
+      },
+      {
+        discount: true,
+        discountedPrice: "87.2",
+        expirationDate: "2025-07-21",
+        groceryName: "漢堡",
+        price: "109",
+        storeName: "7-11長興",
+      },
+      {
+        discount: true,
+        discountedPrice: "87.2",
+        expirationDate: "2025-07-21",
+        groceryName: "便當",
+        price: "109",
+        storeName: "7-11長興",
+      },
+    ];
+    // let groceries = JSON.parse(localStorage.getItem('cart')) || [];
+    console.log(groceries);
+    groceries = groceries.map((item) => ({ ...item, checked: true }));
+    return {
+      groceries,
+    };
+  },
+  computed: {
+    totalPrice() {
+      return this.groceries
+        .filter((item) => item.checked)
+        .reduce((acc, item) => acc + parseInt(item.discountedPrice), 0);
+    },
+  },
+  methods: {
+    toPayPage() {
+      window.location.href = "pay";
+    },
+    toHomePage() {
+      window.location.href = "home";
+    },
+    removeItem(index) {
+      this.groceries.splice(index, 1);
+      // localStorage.setItem("cart", JSON.stringify(this.groceries));
+    },
+    getImageUrl(groceryName) {
+      const images = {
+        飯糰: require("../assets/onigiri.png"),
+        麵包: require("../assets/bread.png"),
+        漢堡: require("../assets/burger.png"),
+        三明治: require("../assets/sandwich.png"),
+        便當: require("../assets/bento.png"),
+      };
+      return images[groceryName] || "images/default.png";
+    },
+  },
 };
 </script>
 
--- front-end/src/views/HomePage.vue
+++ front-end/src/views/HomePage.vue
@@ -1,5 +1,318 @@
 <template>
-  <h1>This is the Home Page</h1>
+  <div class="body">
+    <div class="header-content wrapper-content">
+      <h1 class="logo">HomePage</h1>
+      <div class="search">
+        <input
+          class="search-bar"
+          type="text"
+          name="search"
+          id="search"
+          placeholder="輸入你想尋找的店家"
+        />
+        <button class="search-btn">
+          <i class="fa-solid fa-magnifying-glass"></i>
+        </button>
+      </div>
+
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navDropdown"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navDropdown">
+          <ul class="navbar-nav">
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                data-bs-toggle="dropdown"
+                href="#"
+                ><i class="fa-solid fa-user"></i>User</a
+              >
+              <div class="dropdown-menu">
+                <a
+                  class="dropdown-item"
+                  onclick="window.location.href='loginPage.html'"
+                  >登入</a
+                >
+                <a class="dropdown-item" onclick="logout()">登出</a>
+              </div>
+            </li>
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                data-bs-toggle="dropdown"
+                href="#"
+                ><i class="fa-solid fa-globe"></i>語言</a
+              >
+              <div class="dropdown-menu">
+                <a class="dropdown-item" href="#">繁體中文</a>
+                <a class="dropdown-item" href="#">ENG</a>
+              </div>
+            </li>
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                data-bs-toggle="dropdown"
+                href="#"
+                ><i class="fa-solid fa-globe"></i>購物車</a
+              >
+              <div class="dropdown-menu">
+                <a class="dropdown-item" href="#">我的購物車</a>
+                <a class="dropdown-item" href="#">tmep</a>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+    <main class="content-body">
+      <aside class="side-menu">
+        <div class="side-header">
+          <h4 class="side-title">小道消息</h4>
+          <div class="col-lg-auto right">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+              <button
+                class="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navDropdown"
+              >
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navDropdown">
+                <ul class="navbar-nav">
+                  <li class="nav-item dropdown">
+                    <a
+                      class="nav-link dropdown-toggle"
+                      data-bs-toggle="dropdown"
+                      href="#"
+                    >
+                      <i class="fa-solid fa-sort"></i
+                      ><span id="sortLabel">排序</span>
+                    </a>
+                    <div class="dropdown-menu">
+                      <a
+                        class="dropdown-item"
+                        onclick="sortByPrice('highToLow')"
+                        >價格(低至高)</a
+                      >
+                      <a class="dropdown-item" onclick="sortByPrice()"
+                        >價格(高至低)</a
+                      >
+                      <a
+                        class="dropdown-item"
+                        onclick="sortByDistance('highToLow')"
+                        >距離(近到遠)</a
+                      >
+                      <a class="dropdown-item" onclick="sortByDistance()"
+                        >距離(遠到近)</a
+                      >
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </nav>
+          </div>
+        </div>
+        <div class="side-body">
+          <div
+            class="card mb-3"
+            style="max-width: 600px"
+            onclick="redirectToDetail()"
+          >
+            <div class="row g-0">
+              <div class="pic col-md-4">
+                <img
+                  src="https://th.bing.com/th/id/OIP.d4b5Mivbu8__5GpelvRekQHaF7?rs=1&pid=ImgDetMain"
+                  class="img-fluid rounded-start"
+                  alt="..."
+                />
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h5 class="card-title">艋舺雞排</h5>
+                  <p class="card-text">79</p>
+                  <p class="card-footer">
+                    <small class="text-body-secondary"
+                      >106台北市大安區敦化南路二段331巷14號</small
+                    >
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div
+            class="card mb-3"
+            style="max-width: 600px"
+            onclick="redirectToDetail()"
+          >
+            <div class="row g-0">
+              <div class="pic col-md-4">
+                <img
+                  src="https://www.woaisha.com/wp-content/uploads/2021/04/%E7%85%A7%E7%87%92%E8%B1%9A%E8%82%89%E4%BE%BF%E7%95%B6-641.jpg"
+                  class="img-fluid rounded-start"
+                  alt="..."
+                />
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h5 class="card-title">大安區某200塊便當</h5>
+                  <p class="card-text">180</p>
+                  <p class="card-footer">
+                    <small class="text-body-secondary"
+                      >106台北市大安區基隆路三段85號</small
+                    >
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div
+            class="card mb-3"
+            style="max-width: 600px"
+            onclick="redirectToDetail()"
+          >
+            <div class="row g-0">
+              <div class="pic col-md-4">
+                <img
+                  src="https://image.taiwantoday.tw/images/content_info/img20240116151958055.jpg"
+                  class="img-fluid rounded-start"
+                  alt="..."
+                />
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h5 class="card-title">新店區某100塊的麵</h5>
+                  <p class="card-text">88</p>
+                  <p class="card-footer">
+                    <small class="text-body-secondary"
+                      >231新北市新店區中正路542之5號1樓</small
+                    >
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="side-footer">
+          <button id="toAddPageButton" onclick="toAddPage()">
+            <i class="fa-solid fa-plus"></i>
+          </button>
+        </div>
+      </aside>
+
+      <section class="secondary-content wrapper-content">
+        <h2 class="secondary__title section__title"></h2>
+        <div class="row mx-4 pt-6 ps-4">
+          <div
+            class="wrapper-work"
+            data-store="7-11"
+            onclick="redirectToStore('7-11')"
+          >
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/7-eleven_logo.svg/250px-7-eleven_logo.svg.png"
+              alt=""
+            />
+            <h3 class="work_title">7-11</h3>
+          </div>
+          <div
+            class="wrapper-work"
+            data-store="全家"
+            onclick="redirectToStore('全家')"
+          >
+            <img
+              src="https://static.104.com.tw/b_profile/cust_picture/5268/130000000165268/logo.png?v=20210418003147"
+              alt=""
+            />
+            <h3 class="work_title">全家</h3>
+          </div>
+          <div
+            class="wrapper-work"
+            data-store="萊爾富"
+            onclick="redirectToStore('萊爾富')"
+          >
+            <img
+              src="https://upload.wikimedia.org/wikipedia/zh/5/54/Hi-Life.svg"
+              alt=""
+            />
+            <h3 class="work_title">萊爾富</h3>
+          </div>
+          <div
+            class="wrapper-work"
+            data-store="OK"
+            onclick="redirectToStore('OK')"
+          >
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/OK_LOGO-%E6%96%B9%E5%BD%A2-01.png/220px-OK_LOGO-%E6%96%B9%E5%BD%A2-01.png"
+              alt=""
+            />
+            <h3 class="work_title">OK</h3>
+          </div>
+          <div
+            class="wrapper-work"
+            data-store="全聯"
+            onclick="redirectToStore('全聯')"
+          >
+            <img
+              src="https://curation.culture.tw/api/pic?id=User022cdfcd8-a336-483b-83dc-7a89288f6863&w=1000"
+              alt=""
+            />
+            <h3 class="work_title">全聯</h3>
+          </div>
+          <div
+            class="wrapper-work"
+            data-store="其他店家"
+            onclick="redirectToStore('其他店家')"
+          >
+            <img
+              src="https://storage.googleapis.com/opinion-cms-cwg-tw/article/201808/article-5b75318dc7be2.jpg"
+              alt=""
+            />
+            <h3 class="work_title">其他店家</h3>
+          </div>
+          <div
+            class="wrapper-work"
+            data-store="其他店家"
+            onclick="redirectToStore('其他店家')"
+          >
+            <img
+              src="https://storage.googleapis.com/opinion-cms-cwg-tw/article/201808/article-5b75318dc7be2.jpg"
+              alt=""
+            />
+            <h3 class="work_title">其他店家</h3>
+          </div>
+          <div
+            class="wrapper-work"
+            data-store="其他店家"
+            onclick="redirectToStore('其他店家')"
+          >
+            <img
+              src="https://storage.googleapis.com/opinion-cms-cwg-tw/article/201808/article-5b75318dc7be2.jpg"
+              alt=""
+            />
+            <h3 class="work_title">其他店家</h3>
+          </div>
+          <div
+            class="wrapper-work"
+            data-store="其他店家"
+            onclick="redirectToStore('其他店家')"
+          >
+            <img
+              src="https://storage.googleapis.com/opinion-cms-cwg-tw/article/201808/article-5b75318dc7be2.jpg"
+              alt=""
+            />
+            <h3 class="work_title">其他店家</h3>
+          </div>
+        </div>
+      </section>
+    </main>
+  </div>
 </template>
 
 <script>
@@ -9,7 +322,322 @@ export default {
 </script>
 
 <style scoped>
-h1 {
-  color: cornflowerblue;
+.body {
+  margin: 0;
+  font-family: "Noto Sans", "Microsoft JhengHei";
+  letter-spacing: normal;
+  background-color: rgba(144, 189, 231, 0.479);
 }
+a {
+  text-decoration: none;
+}
+/* 下面是頁首的設定 */
+.wrap-content {
+  box-sizing: border-box;
+  width: 1024px;
+}
+.header-content {
+  position: sticky;
+  top: 0;
+  background-color: rgb(144, 189, 231);
+  border-bottom: 2px solid rgb(10, 90, 136);
+  height: 70px;
+  padding-left: 30px;
+  padding-right: 30px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.search {
+  position: relative;
+  float: left;
+}
+.search-bar {
+  width: 90%;
+  height: 32px;
+  font-size: 20px;
+  border: 3px solid #3d1101;
+  background-color: #efe9e7;
+}
+.search-btn {
+  width: 36px;
+  height: 32px;
+  background-color: chocolate;
+  color: #efe9e7;
+  outline: none;
+  border: 3px solid #3d1101;
+  cursor: pointer;
+  position: absolute;
+  top: 0;
+  right: 0;
+}
+
+.navbar {
+  border: #ffb500 3px solid;
+}
+.collapse {
+  background-color: #3d1101;
+}
+.nav-item {
+  background-color: wheat;
+}
+.dropdown-item {
+  background-color: wheat;
+}
+.logo {
+  font-family: "Fira Sans Extra Condensed", sans-serif;
+  text-decoration: underline;
+}
+.link-text:hover {
+  transition: all 0.2s;
+  color: #ffb500;
+}
+.content-img {
+  margin: 0 auto;
+  text-align: center;
+}
+/* 下面是文章主要區塊的設定 */
+main {
+  display: flex;
+}
+/* 左邊的po文欄 */
+.side-menu {
+  position: sticky;
+  top: 70px;
+  display: flex;
+  flex-direction: column;
+  height: calc(100vh - 70px);
+  float: left;
+  /* border:3px solid #000; */
+  background: white;
+  margin-bottom: auto;
+  padding: 0 0 10px 0;
+  /* overflow: visible;
+  transition: 0.5s; */
+}
+label {
+  width: 20px;
+  height: 80px;
+  background-color: #d1d1d1;
+  color: #686666;
+  position: absolute;
+  right: -20px;
+  top: 0;
+  bottom: 0;
+  margin: auto;
+  line-height: 80px;
+  text-align: center;
+  border-radius: 0 5px 5px 0;
+  box-shadow: 5px 0 5px rgba(23, 23, 54, 0.6);
+}
+/* #side-menu-active:checked + .side-menu {
+  transform: translateX(-100%);
+  transition: 0.5s;
+
+  label .fas {
+    transform: scaleX(-1);
+  }
+} */
+#side-menu-active {
+  position: absolute;
+  opacity: 0;
+  z-index: -1;
+}
+
+.side-header {
+  padding: 10px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  float: left;
+}
+.side-title {
+  background-color: #4e7d8b;
+}
+/* 每個品項的設定 */
+.side-body {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  float: left;
+  border: 2px solid #000;
+  background: whitesmoke;
+  margin-bottom: auto;
+  padding: 30px 30px 30px 30px;
+  margin-left: 10px;
+  margin-right: 10px;
+  overflow: auto;
+}
+.side-footer {
+  padding: 10px;
+  margin-top: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.side-footer button {
+  border-radius: 10px;
+  height: 150%;
+  padding-left: 15px;
+  padding-right: 15px;
+}
+.side-footer button:hover {
+  box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.9);
+  transition: 0.2s;
+}
+.card {
+  border: none !important;
+  padding: 3px;
+}
+.card .title {
+  color: #444444;
+}
+.card .text {
+  color: #444444;
+}
+.card .pic {
+  height: 100%;
+}
+.card .pic img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+.card .card-body {
+  background-color: #f1f6fa !important;
+}
+.card-footer {
+  margin-bottom: -10px;
+}
+.card-footer p {
+  margin-bottom: 0;
+}
+
+/* 右邊區域 */
+.secondary-content {
+  float: left;
+  width: 70%;
+  padding: 10px 0px 10px 0px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.work_title {
+  font-size: 24px;
+  margin-top: 20px;
+  margin-bottom: 10px;
+}
+.wrapper-work {
+  width: min-content;
+  border-radius: 10px;
+  break-inside: avoid;
+  background: rgb(255, 255, 255);
+  padding: 5px;
+  margin: 1em 1em 1em 1em;
+}
+.wrapper-work:hover {
+  box-shadow: 5px 8px 2px rgba(0, 0, 0, 0.7);
+  transition: 0.5s;
+}
+.wrapper-work img {
+  width: 236px;
+  height: 192px;
+}
+.wrapper-work h3 {
+  text-align: center;
+}
+@media screen and (min-width: 320px) and (max-width: 768px) {
+  /* 判斷螢幕大小 還沒做 */
+  .logo {
+    font-size: small;
+    margin-left: -20px;
+  }
+  .header-content .navbar {
+    margin-right: -40px;
+  }
+  /* .side-menu {
+    position: sticky;
+    top: 70px;
+    display: flex;
+    flex-direction: column;
+    width: 90%;
+    height: calc(100vh - 70px);
+    float: left;
+    border:3px solid #000;
+    background: white;
+    margin-bottom: auto;
+    padding: 0 0 10px 0;
+    z-index: 2;
+  }
+  .secondary-content {
+    position: absolute;
+    z-index: -1;
+    justify-content: center;
+  } */
+}
+
+/* .header-nav {
+  border: #ffb500 3px solid;
+  background-color:wheat;
+  padding: 10px;
+  width: 400px;
+  display: flex;
+  justify-content: space-evenly;
+} */
+/* ul {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  margin: 0;
+  padding: 0;
+  width: 500px;
+  font-size: large;
+}
+ul li{
+  list-style: none;
+  border: #262626 3px solid;
+  transition: 0.5s;
+}
+ul li a {
+  background: beige;
+  display: block;
+}
+ul li a span {
+  color: #262626;
+  position: relative;
+  display: inline-block;
+}
+ul li a span:nth-child(1) {
+  background-color: white;
+  width: 30px;
+  text-align: center;
+  padding: 20px;
+  transition: 0.5s;
+  color: #262626;
+  border-right: #262626 5px solid;
+}
+ul li:hover a span:nth-child(1) {
+  background: #f44336;
+}
+ul li a span:nth-child(1) .fas {
+  transition: 0.5s;
+}
+ul li:hover a span:nth-child(1) .fas {
+  transform: rotateY(360deg);
+  color: #fff;
+}
+ul li a span:nth-child(2) {
+  padding: 10px;
+} */
+
+/* .wrapper-selected-works {
+  display: flex;
+  align-items: stretch;
+  width: 10%;
+  column-width: 250px;
+  column-gap: 40px;
+} */
 </style>
